77c6cb3a725b1f99248355f5d2e5a0be7b279476
azuma196911 <azuma19691119@gmail.com>
2022-11-25 14:50:54 +0900

    v0.2.1

--- main.py
+++ main.py
@@ -3,28 +3,34 @@ This program is a dice program
 
 """
 
-import random
+import sys, random
+
 dice_num1 = 0
 dice_num2 = 0
 
 def main():
-    print('dice program')    
-    var = input("Please input : ")
+    print('dice program')  
 
-    if var == '1d6':
-        dice_num1 = random.randrange(1, 6, 1)
-        print('%s = %d' %(var, dice_num1))
-    
-    if var == '2d6':
-        dice_num1 = random.randrange(1, 6, 1)
-        dice_num2 = random.randrange(1, 6, 1)
-        tmp = dice_num1 + dice_num2
-        print('%s = %d + %d = %d' %(var, dice_num1, dice_num2, tmp))
+    while True:
+        print('Please input')
+        var = input('1d6 or 2d6 or exit : ')
 
+        if var == '1d6':
+            dice_num1 = random.randrange(1, 6, 1)
+            print('%s = %d' %(var, dice_num1))
+        
+        elif var == '2d6':
+            dice_num1 = random.randrange(1, 6, 1)
+            dice_num2 = random.randrange(1, 6, 1)
+            tmp = dice_num1 + dice_num2
+            print('%s = %d + %d = %d' %(var, dice_num1, dice_num2, tmp))
+            if tmp == 2:
+                print('！！ファンブル！！')
+        elif var == 'exit':
+            sys.exit()
 
-
-    else:
-        print("エラー")
+        else:
+            print("エラー")
 
 
 
