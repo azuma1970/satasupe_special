982e0d0b1f3c579a29f71edd7fcd9c40930e4a75
azuma196911 <azuma19691119@gmail.com>
2022-11-25 14:35:20 +0900

    v0.1.4

--- main.py
+++ main.py
@@ -4,16 +4,18 @@ This program is a dice program
 """
 
 import random
-dice_num = 0
-
+dice_num1 = 0
+dice_num2 = 0
 
 def main():
     print('dice program')    
     var = input("Please input : ")
 
     if var == '1d6':
-        dice_num = random.randrange(1, 6, 1)
-        print(dice_num)
+        dice_num1 = random.randrange(1, 6, 1)
+        print('%s = %d' %(var, dice_num1))
+    
+
 
     else:
         print("エラー")
