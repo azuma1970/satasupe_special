dea0fd799a56f108eb714541684548bf70a44dd6
azuma196911 <azuma19691119@gmail.com>
2022-11-25 14:23:26 +0900

    v0.1.2

--- main.py
+++ main.py
@@ -3,9 +3,19 @@ This program is a dice program
 
 """
 
+import random
+#dice_num = 0
+
 def main():
     print('dice program')
+    """
     var = input("Please input : ")
     print(var)
+    """
+    dice_num = random.randrange(1, 6, 1)
+    print(dice_num)
+
+
+
 if __name__ == "__main__":
     main()
