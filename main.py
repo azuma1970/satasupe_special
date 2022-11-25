075fc96f179cfc47c02a3a7988c5044cba832288
azuma196911 <azuma19691119@gmail.com>
2022-11-25 16:29:44 +0900

    v0.5

--- main.py
+++ main.py
@@ -1,39 +1,53 @@
 """ 
 This program is a dice program
 
+code by yuki.M
 """
 
 import sys, random
 
-dice_num1 = 0
-dice_num2 = 0
+
+def Funk_dice_roll(str_dice):
+    list_int_dice_num = []
+    list_dice = list(str_dice)
+    print(list_dice)
+    dice_num1 = int(list_dice[0])
+    dice_num2 = int(list_dice[2])
+    for i in range(dice_num1):
+        list_int_dice_num.append(random.randrange(1, dice_num2, 1))
+    return list_int_dice_num
 
 def main():
     print('dice program')  
 
     while True:
         print('Please input')
-        var = input('1d6 or 2d6 or exit : ')
+        str_dice = input('1d6 or 2d6 or exit : ')
 
-        if var == '1d6':
-            dice_num1 = random.randrange(1, 6, 1)
-            print('%s = %d\n' %(var, dice_num1))
+        if str_dice == '1d6':
+            list_int_dice_num = Funk_dice_roll(str_dice)
+            print('{} = {}\n'.format(str_dice, list_int_dice_num))
+
         
-        elif var == '2d6':
-            dice_num1 = random.randrange(1, 6, 1)
-            dice_num2 = random.randrange(1, 6, 1)
-            tmp = dice_num1 + dice_num2
-            print('%s = %d + %d = %d\n' %(var, dice_num1, dice_num2, tmp))
-            if tmp == 2:
+        elif str_dice == '2d6':
+            Funk_dice_roll(str_dice)
+            list_int_dice_num = Funk_dice_roll(str_dice)
+            tmp = 0
+            result = 0
+            for tmp in list_int_dice_num:
+                result = tmp + result
+            tmp1 = list_int_dice_num[0]
+            tmp2 = list_int_dice_num[1]
+            print('%s = %d + %d = %d\n' %(str_dice, tmp1, tmp2, result))
+            if result == 2:
                 print('！！ファンブル！！\n')
-        elif var == 'exit':
+        elif str_dice == 'exit':
             sys.exit()
 
         else:
             print('エラー\n')
 
 
-
-
 if __name__ == "__main__":
     main()
+
