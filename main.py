cdc90e84b5b55dab6b20045f609c88ad1bdab8a7
azuma196911 <azuma19691119@gmail.com>
2022-11-25 14:57:05 +0900

    v0.2.1

--- main.py
+++ main.py
@@ -17,20 +17,20 @@ def main():
 
         if var == '1d6':
             dice_num1 = random.randrange(1, 6, 1)
-            print('%s = %d' %(var, dice_num1))
+            print('%s = %d\n' %(var, dice_num1))
         
         elif var == '2d6':
             dice_num1 = random.randrange(1, 6, 1)
             dice_num2 = random.randrange(1, 6, 1)
             tmp = dice_num1 + dice_num2
-            print('%s = %d + %d = %d' %(var, dice_num1, dice_num2, tmp))
+            print('%s = %d + %d = %d\n' %(var, dice_num1, dice_num2, tmp))
             if tmp == 2:
-                print('！！ファンブル！！')
+                print('！！ファンブル！！\n')
         elif var == 'exit':
             sys.exit()
 
         else:
-            print("エラー")
+            print('エラー\n')
 
 
 
